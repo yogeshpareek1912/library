111fbab7b733f1abe3798da2f919d7fe0323c219
yogeshpareek1912 <69826978+yogeshpareek1912@users.noreply.github.com>
2020-08-18 13:44:46 -0400

    Add files via upload

---
+++ Student_/footer.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<style type="text/css">
+	footer
+	{
+		height: 200px;
+		width: 1361px;
+		background-color: black;
+	}
+		.fa
+		{
+			margin: 0px 5px;
+			padding: 5px;
+			font-size: 20px;
+			width: 20px;
+			height: 20px;
+			text-align: center;
+			text-decoration: none;
+			border-radius: 50%;
+		}
+		.fa:hover
+		{
+			opacity: .7;
+		}
+		.fa-facebook
+		{
+			background: #3B5998;
+			color: white;
+		}
+		.fa-twitter
+		{
+			background: #55ACEE;
+			color: white;
+		}
+		.fa-google
+		{
+			background: #dd4b39;
+			color: white;
+		}
+		.fa-instagram
+		{
+			background: #125688;
+			color: white;
+		}
+		.fa-yahoo
+		{
+			background: #400297;
+			color: white;
+		}
+	</style>
+
+</head>
+<body>
+<footer style="background-color: black; ">
+	<br>
+	<h3 style="color:white;text-align: center;">Contact us through social media</h3><br>
+
+	<div style="text-align: center;">
+
+		<a href="https://www.facebook.com/Yogesh1912" class="fa fa-facebook"></a>
+		<a href="https://www.instagram.com/p/CBJ51Gtjn-0vzroqcQLECtSy8vwRzdr8-aQqbI0/?igshid=la2z34vfetzw" class="fa fa-instagram"></a>
+		<a href="https://www.linkedin.com/in/yogesh-pareek-ba86651a9" class="fa fa-linkedin"></a>
+		<a href="Yogeshpareek1912@gmail.com" class="fa fa-google"></a>
+
+
+	</div>
+
+	<br>
+	<p style="color:white;text-align: center;">
+		<br>
+		Email:&nbsp Online.library@gmail.com <br><br>
+		Mobile:&nbsp &nbsp +880171*******
+	</p>
+</footer>
+</body>
+</html>
